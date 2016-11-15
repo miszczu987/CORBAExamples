96bc9ec81378882f101ee7ffaad093525f5a94ff
Piotr Rydz <rydzp1987@gmail.com>
2016-11-15 23:45:54 +0100

    Client...

--- P1_Basic/Client/src/Client.cpp
+++ P1_Basic/Client/src/Client.cpp
@@ -34,10 +34,11 @@ int main(int argc, char* argv[])
 		CORBAHello::Echo_var server = CORBAHello::Echo::_narrow(serverRef.in());
 
 		CONSOLE("Send message to Server");
-		CORBA::String_var echoString = server->sendMsg("Hello from Client");
+		CORBA::String_var echoString = server->sendMsg("Hello from Client!");
 
-		CONSOLE("Got echo from Server: " << echoString.in());
+		CONSOLE("Response from Server: " << echoString.in());
 
+		CONSOLE("Destroy ORB");
 		theORB->destroy();
 	}
 	catch(const CORBA::Exception& e)
